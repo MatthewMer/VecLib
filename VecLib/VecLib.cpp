b27d335c7aec02dee8aded17027d5d02feac1d0a
Matthias Merkl <matthias-merkl@gmx.de>
2024-06-06 20:03:16 +0200

    Initial commit, currently only Vec3 with basic, untested operations

---
+++ VecLib/VecLib.cpp
@@ -0,0 +1,102 @@
+// VecLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include "VecLib.h"
+
+#include <cmath>
+#define _USE_MATH_DEFINES
+
+namespace VecLib {
+	namespace {
+		const double pi = 3.14159265358979323846;
+	}
+
+	Vec3& Vec3::operator=(const Vec3& rhs) {
+		return *this;
+	}
+
+	Vec3& Vec3::operator+=(const Vec3& rhs) {
+		return *this;
+	}
+
+	Vec3& Vec3::operator-=(const Vec3& rhs) {
+		return *this;
+	}
+
+	const Vec3& Vec3::operator+(const Vec3& rhs) {
+		return Vec3(
+			this->x + rhs.x, 
+			this->y + rhs.y, 
+			this->z + rhs.z
+		);
+	}
+
+	const Vec3& Vec3::operator-(const Vec3& rhs) {
+		return Vec3(
+			this->x - rhs.x,
+			this->y - rhs.y,
+			this->z - rhs.z
+		);
+	}
+
+	template<typename T>
+	const Vec3& Vec3::operator/(const T& rhs) {
+		return Vec3(
+			this->x / rhs,
+			this->y / rhs,
+			this->z / rhs
+		);
+	}
+
+	template<typename T>
+	const Vec3& Vec3::operator*(const T& rhs) {
+		return Vec3(
+			this->x * rhs,
+			this->y * rhs,
+			this->z * rhs
+		);
+	}
+
+	template<typename T>
+	Vec3& Vec3::operator/=(const T& rhs) {
+		return *this;
+	}
+
+	template<typename T>
+	Vec3& Vec3::operator*=(const T& rhs) {
+		return *this;
+	}
+
+	float Vec3::magnitude() const {
+		return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	}
+
+	float Vec3::dot(const Vec3& in) {
+		return this->x * in.x + this->y * in.y + this->z * in.z;
+	}
+
+	Vec3 Vec3::cross(const Vec3& in) {
+		return Vec3(
+			this->y * in.z - in.y * this->z,
+			this->z * in.x - in.z * this->x,
+			this->x * in.y - in.x * this->y
+		);
+	}
+
+	float Vec3::anglerad(const Vec3& in) {
+		float res = acos(this->dot(in) / (this->magnitude() * in.magnitude()));
+		if (res > pi)	{ return res - pi; }
+		else			{ return res; }
+	}
+
+	float Vec3::angledeg(const Vec3& in) {
+		return anglerad(in) * 180.f / pi;
+	}
+
+	void Vec3::normalize() {
+		*this /= this->magnitude();
+	}
+}
---
+++ VecLib/VecLib.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace VecLib {
+	class Vec3 {
+	public:
+		float x;
+		float y;
+		float z;
+
+		Vec3():x(0),y(0),z(0){}
+		Vec3(float x, float y, float z) :x(x), y(y), z(z) {}
+		Vec3(const Vec3& in){
+			*this = in;
+		}
+
+		Vec3& operator=(const Vec3& rhs);
+
+		Vec3& operator+=(const Vec3& rhs);
+		Vec3& operator-=(const Vec3& rhs);
+
+		const Vec3& operator+(const Vec3& rhs);
+		const Vec3& operator-(const Vec3& rhs);
+
+		template<typename T>
+		const Vec3& operator/(const T& rhs);
+		template<typename T>
+		const Vec3& operator*(const T& rhs);
+
+		template<typename T>
+		Vec3& operator/=(const T& rhs);
+		template<typename T>
+		Vec3& operator*=(const T& rhs);
+
+		float magnitude() const;
+		float dot(const Vec3& in);
+		Vec3 cross(const Vec3& in);
+		float anglerad(const Vec3& in);
+		float angledeg(const Vec3& in);
+		void normalize();
+	};
+}
